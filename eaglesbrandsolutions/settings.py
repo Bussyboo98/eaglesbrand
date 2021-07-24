c6dcae365df4c385dd268027924984c48a058093
Benwa78 <nonwaz78@gmail.com>
2021-07-24 08:01:31 -0400

    My message

--- eaglesbrandsolutions/settings.py
+++ eaglesbrandsolutions/settings.py
@@ -16,10 +16,10 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/eaglyxow/development.eaglesbrandsolutions.com/static'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [STATIC_DIR,]
+
 
 
 
