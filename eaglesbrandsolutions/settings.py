52cb86d8b7bb56a22f1faf2f0a0cf08336440441
Benwa78 <nonwaz78@gmail.com>
2021-07-24 08:19:36 -0400

    Last message

--- eaglesbrandsolutions/settings.py
+++ eaglesbrandsolutions/settings.py
@@ -17,8 +17,10 @@ from django.contrib.messages import constants as messages
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_ROOT = '/home/eaglyxow/development.eaglesbrandsolutions.com/static'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [STATIC_DIR,]
 
 
 
