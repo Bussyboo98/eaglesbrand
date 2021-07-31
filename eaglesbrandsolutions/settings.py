7649d69075e70c6d8d0dec6c308018dff8ba4701
Benwaz78 <nonwaz78@gmail.com>
2021-07-31 05:10:42 +0100

    My commit

--- eaglesbrandsolutions/settings.py
+++ eaglesbrandsolutions/settings.py
@@ -103,13 +103,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ogunburebusayo.j@gmail.com'
-EMAIL_HOST_PASSWORD = 'bussyboo98'
 
 
 
